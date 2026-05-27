55551055c2c21a1521e64badb037c7ec79548734
Hiokz <weijia056@gmail.com>
2026-05-27 21:18:59 +0800

    Comment out unused backend and template code to display clean static frontend

--- WebServer.py
+++ WebServer.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import render_template
 #https://flask.palletsprojects.com/en/3.0.x/deploying/eventlet/
-import eventlet
-from eventlet import wsgi
-from flask_socketio import SocketIO
-from flask_socketio import emit
+# import eventlet
+# from eventlet import wsgi
+# from flask_socketio import SocketIO
+# from flask_socketio import emit
 app = Flask(__name__)
-#socketio = SocketIO(app)
-socketio = SocketIO(app, async_mode='eventlet')
+# #socketio = SocketIO(app)
+# socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def index():
@@ -37,69 +37,70 @@ def aboutus():
 def task():
    return render_template('task.html')
 
-@socketio.event
-def PestDetection(RxData):
-  socketio.emit('PestDetect', RxData)
-  print('Receive Pest Data from BBBW')
+# @socketio.event
+# def PestDetection(RxData):
+#   socketio.emit('PestDetect', RxData)
+#   print('Receive Pest Data from BBBW')
 
-@socketio.event
-def BBBW1Event(RxData):
+# @socketio.event
+# def BBBW1Event(RxData):
     print(f'Emitting data to Web_BBBW1Event: {RxData}')
-    socketio.emit('Web_BBBW1Event', RxData)
-    print('Receive Data from BBBW1')
+#     socketio.emit('Web_BBBW1Event', RxData)
+#     print('Receive Data from BBBW1')
 
-@socketio.event
-def BBBW2Event(RxData):
+# @socketio.event
+# def BBBW2Event(RxData):
     print(f'Emitting data to Web_BBBW2Event: {RxData}')
-    socketio.emit('Web_BBBW2Event', RxData)
-    print('Receive Data from BBBW2')
+#     socketio.emit('Web_BBBW2Event', RxData)
+#     print('Receive Data from BBBW2')
 
-@socketio.event
-def BBBW3Event(RxData):
+# @socketio.event
+# def BBBW3Event(RxData):
     print(f'Emitting data to Web_BBBW3Event: {RxData}')
-    socketio.emit('Web_BBBW3Event', RxData)
-    print('Receive Data from BBBW3')
+#     socketio.emit('Web_BBBW3Event', RxData)
+#     print('Receive Data from BBBW3')
 
-@socketio.event
-def BBBW4Event(RxData):
+# @socketio.event
+# def BBBW4Event(RxData):
     print(f'Emitting data to Web_BBBW4Event: {RxData}')
-    socketio.emit('Web_BBBW4Event', RxData)
-    print('Receive Data from BBBW4')
+#     socketio.emit('Web_BBBW4Event', RxData)
+#     print('Receive Data from BBBW4')
 
-@socketio.event
-def BBBW5Event(RxData):
+# @socketio.event
+# def BBBW5Event(RxData):
     print(f'Emitting data to Web_BBBW5Event: {RxData}')
-    socketio.emit('Web_BBBW5Event', RxData)
-    print('Receive Data from BBBW5')
+#     socketio.emit('Web_BBBW5Event', RxData)
+#     print('Receive Data from BBBW5')
 
-@socketio.event
-def BBBW6Event(RxData):
+# @socketio.event
+# def BBBW6Event(RxData):
     print(f'Emitting data to Web_BBBW6Event: {RxData}')
-    socketio.emit('Web_BBBW6Event', RxData)
-    print('Receive Data from BBBW6')
+#     socketio.emit('Web_BBBW6Event', RxData)
+#     print('Receive Data from BBBW6')
 
 # Fire Alarm System (234518D)
-@socketio.event
-def ControlUSR0Led(RxData):
-    socketio.emit('ControlUSR0Led', RxData)
-    print('Manual Call Point Reset')
+# @socketio.event
+# def ControlUSR0Led(RxData):
+#     socketio.emit('ControlUSR0Led', RxData)
+#     print('Manual Call Point Reset')
 
-@socketio.event
-def flame_click(RxData):
-    socketio.emit('flame_click', RxData)
-    print('Receive Data from Flame Sensor', RxData)
+# @socketio.event
+# def flame_click(RxData):
+#     socketio.emit('flame_click', RxData)
+#     print('Receive Data from Flame Sensor', RxData)
 
-@socketio.event
-def tamper_click(RxData):
-    socketio.emit('tamper_click', RxData)
-    print('Receive Data from tamper click', RxData)
+# @socketio.event
+# def tamper_click(RxData):
+#     socketio.emit('tamper_click', RxData)
+#     print('Receive Data from tamper click', RxData)
   
-@socketio.event
-def DoorDetection(RxData):
-    socketio.emit('DoorDetection', RxData)
-    print('Received status from Door:', RxData)
+# @socketio.event
+# def DoorDetection(RxData):
+#     socketio.emit('DoorDetection', RxData)
+#     print('Received status from Door:', RxData)
 
 if __name__ == '__main__':
 #app.run(host='192.168.0.X')
-  wsgi.server(eventlet.listen(("172.27.145.141", 5000)), app)
+    app.run(debug=True)
+#   wsgi.server(eventlet.listen(("172.27.145.141", 5000)), app)
 
